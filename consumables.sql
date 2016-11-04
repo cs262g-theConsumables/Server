8869ecaa50e443e7adb1331266259404931d2160
meliornox <meliornox@gmail.com>
2016-11-04 00:32:46 -0400

    Update consumables.sql
    Fixed whitespace oddities

--- consumables.sql
+++ consumables.sql
@@ -113,21 +113,17 @@ INSERT INTO Date VALUES ('jgb23', 'jsk44', false, false, 'Fish House', 'Get Coff
 INSERT INTO Date VALUES ('jsk44', 'aaa11', true, false, 'Spoelhof Center', 'Work Out', '2016-11-12 13:14:15');
 
 --Sample queries
-
 --Return the matches for a student that haven't been invalidated (neither student's validation code is 2 or greater)
-
 SELECT *
 FROM Match
 WHERE aValid < 2 AND bValid < 2 AND ( aCalvinID = 'aaa11' OR bCalvinID = 'aaa11');
 
 --return a student's profile record and their photo
-
 SELECT *
 FROM Student, StudentPicture
 WHERE Student.CalvinID = 'aaa11';
 
 --return all of a student's messages in chronological order with most recent first
-
 SELECT *
 FROM Message
 WHERE toID = 'aaa11' OR fromID = 'aaa11'
